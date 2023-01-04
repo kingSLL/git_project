1bacf995c0f9dc2a8ffeb497455f00802be64ec9
kingSLL <as1120366691@gmail.com>
2023-01-04 16:27:56 +0800

    更新

---
+++ demo/src/components/reusable_components/icon_cps.vue
@@ -0,0 +1,60 @@
+<!-- ==========template=============-->
+<template>
+  <div class="icon">
+    <div class="album">
+      <div class="img_div">
+        <img :src="picUrl" alt="" />
+        <div class="effect mask"></div>
+      </div>
+
+      <div class="details">
+        <p class="song_title clickable">{{ name }}</p>
+        <p class="author clickable">{{ author }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+<!-- ===========script============== -->
+<script setup>
+const props = defineProps({
+  picUrl: String,
+  name: String,
+  author: String,
+  height: {
+    type: String,
+    defualt: "100px",
+  },
+});
+console.log(props.height);
+</script>
+<!-- ============style============== -->
+<style lang="less" scoped>
+.album {
+  .img_div {
+    position: relative;
+    img {
+      height: var(height);
+    }
+    .effect {
+      top: 0;
+      position: absolute;
+      width: 120px;
+      height: var(height);
+      background-position-x: 0px;
+      background-position-y: -570px;
+    }
+  }
+
+  .details {
+    width: 100px;
+    color: #666;
+    margin-top: 5px;
+    overflow: hidden;
+    .song_title {
+      display: inline-block;
+      white-space: nowrap;
+      margin-bottom: 5px;
+    }
+  }
+}
+</style>
